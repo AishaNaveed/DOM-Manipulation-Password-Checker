b17950506289c98c07fbf998a96c1911d10adc67
AishaNaveed <aisha@ailogica.com>
2021-11-19 23:43:42 +0000

    refactoring

--- index.js
+++ index.js
@@ -8,55 +8,62 @@ const medium = document.querySelector('.medium');
 const strong = document.querySelector('.strong');
 const txt = document.querySelector('.txt');
 const showBtn = document.querySelector('.showBtn');
+const progressBar = document.querySelector('.progressBar');
 
-input.addEventListener('keyup', function(){
-    if(input.value !== ''){
+input.addEventListener('keyup', function () {
+    const pwdInput = input.value;
+    //const strength = progressBar.value;
+
+    if (pwdInput !== '') {
         bar.style.display = 'block';
         bar.style.display = 'flex';
-        if(input.value.length < 8 && input.value.match(regExpWeak) || input.value.match(regExpMed) || input.value.match(regExpStr))
-            no=1;
-        if((input.value.length >= 8 && input.value.length <= 12) && ((input.value.match(regExpWeak)) && (input.value.match(regExpMed))) || ((input.value.match(regExpMed)) && (input.value.match(regExpStr))) || ((input.value.match(regExpWeak)) && (input.value.match(regExpStr))))
-            no=2;
-        if(input.value.length >= 12 && input.value.match(regExpWeak) && input.value.match(regExpMed) && input.value.match(regExpStr))
-            no=3;
-        if(no == 1){
+        if (pwdInput.length < 8 && pwdInput.match(regExpWeak) || pwdInput.match(regExpMed) || pwdInput.match(regExpStr)) {
+            no = 1;
+        }
+        if ((8 <= pwdInput.length <= 12) && (pwdInput.match(regExpWeak) && pwdInput.match(regExpMed)) || (pwdInput.match(regExpMed) && pwdInput.match(regExpStr)) || (pwdInput.match(regExpWeak) && pwdInput.match(regExpStr))) {
+            no = 2;
+        }
+        if (pwdInput.length >= 12 && pwdInput.match(regExpWeak) && pwdInput.match(regExpMed) && pwdInput.match(regExpStr)) {
+            no = 3;
+        }
+        if (no == 1) {
             weak.classList.add('active');
             txt.style.display = 'block';
             txt.textContent = 'Should be longer';
             txt.classList.add('weak');
         }
-        if(no == 2){
+        if (no == 2) {
             medium.classList.add('active');
             txt.textContent = 'Pretty good';
             txt.classList.add('medium');
         }
-        else{
+        else {
             medium.classList.remove('active');
             txt.classList.remove('medium');
         }
-        if(no == 3){
+        if (no == 3) {
             medium.classList.add('active');
             strong.classList.add('active');
             txt.textContent = 'Grrrreat!';
             txt.classList.add('strong');
         }
-        else{
+        else {
             strong.classList.remove('active');
             txt.classList.remove('strong');
         }
         showBtn.style.display = 'block';
-        showBtn.addEventListener('click', function(){
-            if(input.type === 'password'){
+        showBtn.addEventListener('click', function () {
+            if (input.type === 'password') {
                 input.type = 'text';
                 showBtn.textContent = 'SHOW';
             }
-            else{
+            else {
                 input.type = 'password';
                 showBtn.textContent = 'HIDE';
             }
         })
     }
-    else{
+    else {
         bar.style.display = 'none';
         txt.style.display = 'none';
         showBtn.style.display = 'none';
